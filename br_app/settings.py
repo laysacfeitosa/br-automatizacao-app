cdd24b6c06374a9275eb69195dff1e9e733b86d9
Laysa Cabedo <laysacfeitosa@gmail.com>
2025-08-28 14:18:30 -0300

    fix: força sslmode=require no Postgres

--- br_app/settings.py
+++ br_app/settings.py
@@ -97,11 +97,14 @@ if DB_NAME:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql",
-            "NAME": DB_NAME,
+            "NAME": os.getenv("DB_NAME"),
             "USER": os.getenv("DB_USER"),
             "PASSWORD": os.getenv("DB_PASSWORD"),
             "HOST": os.getenv("DB_HOST"),
             "PORT": os.getenv("DB_PORT", "5432"),
+            "OPTIONS": {
+                "sslmode": "require",   # <- ESSENCIAL no Render
+            },
         }
     }
 else:
